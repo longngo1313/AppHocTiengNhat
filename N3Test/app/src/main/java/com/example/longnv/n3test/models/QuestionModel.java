5567f80a597dc016742c5f590d6cb007d7022173
Nguyen Vu Long <nguyenvulong2002@gmail.com>
2018-08-08 17:29:45 +0700

    Add Rxjava, RxAndroid to Project

--- N3Test/app/src/main/java/com/example/longnv/n3test/models/QuestionModel.java
+++ N3Test/app/src/main/java/com/example/longnv/n3test/models/QuestionModel.java
@@ -1,5 +1,6 @@
 package com.example.longnv.n3test.models;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 
@@ -9,6 +10,15 @@ import com.example.longnv.n3test.database.DatabaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class QuestionModel extends BaseModel {
 
@@ -19,12 +29,33 @@ public class QuestionModel extends BaseModel {
 
     private DatabaseHelper mDataHelper;
 
+    @SuppressLint("CheckResult")
     public void getListQuestion(){
 
+        getInt().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d("15081991 -onSubscribe- ", "  " + Thread.currentThread().getName());
+                    }
 
-        mDataHelper = new DatabaseHelper(getContext());
-        List<Question> questionList =  mDataHelper.getAllQuetions();
-        Log.d("15081991 " , "" + questionList.size());
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d("15081991 -onNext- ", "  " + Thread.currentThread().getName());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d("15081991 -onComplete- ", "  " + Thread.currentThread().getName());
+                    }
+                });
+
 
 
         DefaultExecutorSupplier.getInstance().forBackgroundTasks().execute(new Runnable() {
@@ -57,4 +88,16 @@ public class QuestionModel extends BaseModel {
         });
 
      }
+
+
+     public Observable<Integer>  getInt(){
+         return Observable.defer(new Callable<ObservableSource<? extends Integer>>() {
+             @Override public Observable<Integer> call() {
+                 Log.d("15081991 Observable", Thread.currentThread().getName() );
+                 return Observable.just(1,2,3,4,5,6,7,8,9,10);
+             }
+         });
+     }
+
+
 }
