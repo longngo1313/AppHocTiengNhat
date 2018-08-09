37525588d0a67a81fba508513e0bf9eb07f4c377
Nguyen Vu Long <nguyenvulong2002@gmail.com>
2018-08-09 16:47:01 +0700

    Update multithread in RX Java

--- N3Test/app/src/main/java/com/example/longnv/n3test/models/QuestionModel.java
+++ N3Test/app/src/main/java/com/example/longnv/n3test/models/QuestionModel.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -29,75 +31,102 @@ public class QuestionModel extends BaseModel {
 
     private DatabaseHelper mDataHelper;
 
+    private Disposable mDisposable;
+
     @SuppressLint("CheckResult")
     public void getListQuestion(){
 
-        getInt().subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Integer>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        Log.d("15081991 -onSubscribe- ", "  " + Thread.currentThread().getName());
-                    }
+//        getInt().subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Observer<Integer>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//                        Log.d("15081991 -onSubscribe- ", "  " + Thread.currentThread().getName());
+//                    }
+//
+//                    @Override
+//                    public void onNext(Integer integer) {
+//                        Log.d("15081991 -onNext- ", "  " + Thread.currentThread().getName() + " data " + integer);
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.d("15081991 -onComplete- ", "  " + Thread.currentThread().getName());
+//                    }
+//                });
 
-                    @Override
-                    public void onNext(Integer integer) {
-                        Log.d("15081991 -onNext- ", "  " + Thread.currentThread().getName());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.d("15081991 -onComplete- ", "  " + Thread.currentThread().getName());
-                    }
-                });
-
-
-
-        DefaultExecutorSupplier.getInstance().forBackgroundTasks().execute(new Runnable() {
+        Observable<List<Question>> listObservable = Observable.fromCallable(new Callable<List<Question>>() {
             @Override
-            public void run() {
-                final ArrayList<Question> questions = new ArrayList<>();
+            public List<Question> call() throws Exception {
+                return queryListQuestion();
+            }
+        });
 
-                Question question = new Question.QuestionBuilder("01")
-                        .setQuestion("Nguyen Vu Long")
-                        .setAnswerOne("One")
-                        .setAnswerTwo("Two")
-                        .setAnswerThree("Three")
-                        .setAnswerFour("Four")
-                        .setRightAnswer("True").build();
-
-                questions.add(question);
-                questions.add(question);
-                questions.add(question);
-                questions.add(question);
-                questions.add(question);
-
-                DefaultExecutorSupplier.getInstance().forMainThreadTasks().execute(new Runnable() {
+        mDisposable = listObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<Question>>() {
                     @Override
-                    public void run() {
+                    public void accept(List<Question> questions) throws Exception {
+                        Log.d("15081991 -accept- ", "  " + Thread.currentThread().getName() + " Data is -- " + questions);
                         getmICallBackPresenter().onCallBackModel("DefaultExecutorSupplier", questions);
                     }
                 });
 
-            }
-        });
 
-     }
+//        DefaultExecutorSupplier.getInstance().forBackgroundTasks().execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                final ArrayList<Question> questions = new ArrayList<>();
+//
+//                Question question = new Question.QuestionBuilder("01")
+//                        .setQuestion("Nguyen Vu Long")
+//                        .setAnswerOne("One")
+//                        .setAnswerTwo("Two")
+//                        .setAnswerThree("Three")
+//                        .setAnswerFour("Four")
+//                        .setRightAnswer("True").build();
+//
+//                questions.add(question);
+//                questions.add(question);
+//                questions.add(question);
+//                questions.add(question);
+//                questions.add(question);
+//
+//                DefaultExecutorSupplier.getInstance().forMainThreadTasks().execute(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        getmICallBackPresenter().onCallBackModel("DefaultExecutorSupplier", questions);
+//                    }
+//                });
+//
+//            }
+//        });
 
+    }
 
-     public Observable<Integer>  getInt(){
-         return Observable.defer(new Callable<ObservableSource<? extends Integer>>() {
-             @Override public Observable<Integer> call() {
-                 Log.d("15081991 Observable", Thread.currentThread().getName() );
-                 return Observable.just(1,2,3,4,5,6,7,8,9,10);
-             }
-         });
-     }
+    private List<Question> queryListQuestion(){
+        ArrayList<Question> questions = new ArrayList<>();
+        Log.d("15081991 query", "  " + Thread.currentThread().getName());
+        Question question = new Question.QuestionBuilder("01")
+                .setQuestion("Nguyen Vu Long")
+                .setAnswerOne("One")
+                .setAnswerTwo("Two")
+                .setAnswerThree("Three")
+                .setAnswerFour("Four")
+                .setRightAnswer("True").build();
+
+        questions.add(question);
+        questions.add(question);
+        questions.add(question);
+        questions.add(question);
+        questions.add(question);
+        return questions;
+    }
 
 
 }
